8d8cb3e6fb946cb044aee632059b7c5edea62b2a
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:36:21 +0400

    upd: engine_init function updated

--- src/engine.c
+++ src/engine.c
@@ -19,21 +19,24 @@ bool engine_init(Engine* engine, const char* title, int width, int height) {
     engine->window = SDL_CreateWindow(title, width, height, SDL_WINDOW_RESIZABLE);
 
     if (!engine->window) {
-        printf("Failed to create window.\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create window: %s", SDL_GetError());
         SDL_Quit();
         return false;
     }
 
-    engine->is_running = true;
-    SDL_Event event;
-
-    while (engine->is_running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                engine->is_running = false;
-            }
-        }
+    engine->renderer = SDL_CreateRenderer(engine->window, NULL);
+    
+    if (!engine->renderer) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(engine->window);
+        SDL_Quit();
+        return false;
     }
+
+    engine->title = title;
+    engine->width = width;
+    engine->height = height;
+    engine->is_running = true;
 
     return true;
 }
