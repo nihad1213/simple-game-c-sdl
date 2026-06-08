85f14b76c4355f38f27a3a4f1866baf2f2340988
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:22:23 +0400

    add: struct documented

--- src/include/engine.h
+++ src/include/engine.h
@@ -3,13 +3,16 @@
 #include <SDL3/SDL.h>
 #include <stdbool.h>
 
+/**
+ * @brief The Engine struct holds the state of the game engine.
+ */
 typedef struct {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    const char* title;
-    int width;
-    int height;
-    bool is_running;
+    SDL_Window* window;         /**< The main game window */
+    SDL_Renderer* renderer;     /**< The renderer for drawing graphics */
+    const char* title;          /**< The title of the game window */
+    int width;                  /**< The width of the game window */
+    int height;                 /**< The height of the game window */ 
+    bool is_running;            /**< Flag to indicate if the engine is running */
 } Engine;
 
 bool engine_init(Engine* engine, const char* title, int width, int height);
