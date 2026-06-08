d33debf9a59f36aefccfec45cbe44413ea1f3b37
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:14:07 +0400

    add: new member added to Engine struct

--- src/include/engine.h
+++ src/include/engine.h
@@ -7,11 +7,12 @@
 typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
+    char* title;
     int width;
     int height;
     bool is_running;
 } Engine;
 
-
+bool engine_init(Engine* engine, const char* title, int width, int height);
 bool engine_window_init();
 void engine_cleanup();
