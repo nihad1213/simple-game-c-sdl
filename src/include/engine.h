fe99879fcc2f6d73887e60880765c5b465a30b7a
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:07:19 +0400

    new: engine.h file first function created

--- src/include/engine.h
+++ src/include/engine.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
 #include <stdbool.h>
 
 typedef struct {
-
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    int width;
+    int height;
 } Engine;
+
+bool engine_window_init();
