b80bb14fcad869c885523b4842653f1d66f1a34a
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:15:49 +0400

    add: Engine member made const function prototypes created

--- src/include/engine.h
+++ src/include/engine.h
@@ -7,12 +7,12 @@
 typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
-    char* title;
+    const char* title;
     int width;
     int height;
     bool is_running;
 } Engine;
 
-bool engine_init(Engine* engine, const char* title, int width, int height);
-bool engine_window_init();
-void engine_cleanup();
+bool engine_init(Engine* engine, char* title, int width, int height);
+bool engine_window_init(Engine* engine, char* title, int width, int height);
+void engine_cleanup(Engine* engine);
