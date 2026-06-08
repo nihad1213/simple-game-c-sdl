aa2c022c8843a3d336b4fcb760f51415f4e00458
Nihad <nihad.nemetli@gmail.com>
2026-06-08 23:41:28 +0400

    add: new functions added to main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <engine.h>
+
 
 int main() {
-    printf("Hello, World");
+    Engine engine;
+
+    if (!engine_init(&engine, "My Game", 800, 600)) {
+        printf("Engine initialization failed!\n");
+        return 1;
+    }
+
+    engine_run(&engine);
+
+    engine_cleanup(&engine);
+    
     return 0;
 }
