0e7c09eb152569087c7ccd42ab7237d047fa9ee2
Nihad <nihad.nemetli@gmail.com>
2026-06-11 00:22:52 +0400

    add: animation enum created

--- include/character.h
+++ include/character.h
@@ -7,6 +7,18 @@
 #define FRAME_HEIGHT 200
 #define IDLE_FRAMES 8
 
+typedef enum {
+    ANIM_IDLE,
+    ANIM_RUN,
+    ANIM_JUMP,
+    ANIM_FALL,
+    ANIM_DEATH,
+    ANIM_ATTACK1,
+    ANIM_ATTACK2,
+    ANIM_TAKE_HIT
+} Animation;
+
+
 
 typedef struct {
 
