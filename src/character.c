a72767e78cd2ddb46ababdb37d331b0a09c1a282
Nihad <nihad.nemetli@gmail.com>
2026-06-12 02:11:23 +0400

    new: load animation function created

--- src/character.c
+++ src/character.c
@@ -1,3 +1,28 @@
 #include <SDL3_image/SDL_image.h>
 #include "character.h"
 
+static bool load_animation(SDL_Renderer* renderer, Animation* anim, const char* path, int frame_count,
+                           int frame_delay_ms) {
+    
+    if (!path) return false;
+
+    SDL_Surface* surface = IMG_Load(path);
+    if (!surface) {
+        SDL_Log("Failed to load sprite!");
+        return false;
+    }
+
+    SDL_SetSurfaceColorKey(surface, true, SDL_MapRGB
+        (SDL_GetPixelFormatDetails(surface->format), NULL, 0, 0, 0));
+
+    anim->sheet = SDL_CreateTextureFromSurface(renderer, surface);
+    anim->frame_count = frame_count;
+    anim->frame_width = surface->w / frame_count;
+    anim->frame_height = surface->h;
+    anim->current_frame = 0;
+    anim->last_frame_time = SDL_GetTicks();
+    anim->frame_delay_ms = frame_delay_ms;
+
+    SDL_DestroySurface(surface);
+    return anim->sheet != NULL;
+}
